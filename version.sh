70d530fe9be98ccf25e2b80b425a2c891b2d9db3
zgll <41069144+zgll@users.noreply.github.com>
2023-05-09 12:46:14 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -2,4 +2,4 @@
 export FRPS_VER=0.20.0
 export FRPS_INIT="https://ghproxy.com/https://raw.githubusercontent.com/clangcn/onekey-install-shell/master/frps/frps.init"
 export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
-export github_download_url="https://ghproxy.com/https://github.com/fatedier/frp/releases/download"
+export github_download_url="https://proxy.yaoyaoling.net/https://github.com/fatedier/frp/releases/download"
